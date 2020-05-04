4b0c639df4cfb4e58040fdef6f94224b894b31a8
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-05-04 14:40:46 +0000

    Providing the router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
