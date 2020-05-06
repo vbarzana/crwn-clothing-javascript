14e4f35f98196b644e6af075c4abe432b977edb5
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-05-06 09:49:23 +0000

    Adding cart icon and ensuring that the cart dropdown menu is expanded/collapsed based on the user clicking in the cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,11 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -28,12 +29,14 @@ const Header = ({ currentUser }) => (
         </Link>
       )}
       <CartIcon />
+      {hidden ? null : <CartDropdown className='transition'/>}
     </div>
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
