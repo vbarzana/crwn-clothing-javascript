1206c4d5aac3fac725f22af5836d93411cbb8c7d
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-05-08 10:10:09 +0000

    Created cartItem component to allow items to be rendered directly in the cart dropdown and include the count of every item quantity

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
-
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items'></div>
+    <div className='cart-items'>
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: {cartItems} }) => ({
+  cartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
