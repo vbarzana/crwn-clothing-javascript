87857792c4f47f1c08f8bb294956391ba67dfea0
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-06-22 10:11:07 +0000

    removing logger from PRD environment as this is not needed.

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 // action -> middleware -> rootReducer -> store -> DOM Changes
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
