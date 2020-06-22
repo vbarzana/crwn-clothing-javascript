81738a8a29728936b0ce2301b9e44416a3495e6c
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-06-22 10:15:19 +0000

    Removing console.log statements that were exposing credentials

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,8 +12,6 @@ const config = {
   appId: process.env.REACT_APP_BACKEND_SENDER_APP_ID,
 };
 
-console.log(config);
-
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
