1a8d07c5207910648dd2dbad5ae32445bd6d19a6
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-09-22 13:53:36 +0000

    Cleaning up collections root saga for cleanliness

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import {
   firestore,
   convertCollectionsSnapshotToMap
@@ -21,9 +21,13 @@ export function* fetchCollectionsAsync() {
   }
 }
 
-export function* fetchCollectionsStart() {
+export function* onFetchCollectionsStart() {
   yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(onFetchCollectionsStart)]);
 }
