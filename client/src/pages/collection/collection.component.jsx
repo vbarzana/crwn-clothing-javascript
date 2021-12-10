3f451aa8f983f4c40f6f40f7ae856d707984c70b
vbarzana <viticoinf@gmail.com>
2021-12-10 07:15:39 +0200

    fix: adding react context, first steps...

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
 
-import CollectionItem from '../../components/collection/collection-item/collection-item.component';
-import { selectCollection } from '../../redux/shop/shop.selectors';
-import { CollectionPageContainer } from './collection.styles';
+import CollectionItem from "../../components/collection/collection-item/collection-item.component";
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+import CollectionsContext from "../../contexts/collections/collections.context";
+
+import { CollectionPageContainer } from "./collection.styles";
+
+const CollectionPage = ({ match }) => {
   return (
-    <CollectionPageContainer>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </CollectionPageContainer>
+    <CollectionsContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <CollectionPageContainer>
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </CollectionPageContainer>
+        );
+      }}
+    </CollectionsContext.Consumer>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
