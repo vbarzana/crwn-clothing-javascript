38911e76c787a2655a151280179d36f908941b81
vbarzana <viticoinf@gmail.com>
2022-07-31 08:43:30 +0300

    fix: Fixing dependencies, adding tsconfig.json

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
-
-import './index.scss';
+import { stripePromise } from './utils/stripe/stripe.utils';
+import * as registerServiceWorker from './serviceWorkerRegistration';
 
 const rootElement = document.getElementById('root');
 
@@ -16,10 +17,14 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
 );
+
+registerServiceWorker.register();
