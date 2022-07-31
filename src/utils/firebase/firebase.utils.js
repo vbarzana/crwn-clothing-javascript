67fb15e8fcaca8900c7cf382ac555be5e90ca84d
vbarzana <viticoinf@gmail.com>
2022-07-31 08:04:44 +0300

    removing dotenv from the code.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,13 +21,13 @@ import {
   getDocs,
 } from "firebase/firestore";
 
-require("dotenv").config();
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  databaseUrl: process.env.REACT_APP_BACKEND_DATABASE_URL,
   appId: process.env.REACT_APP_APP_ID,
 };
 
