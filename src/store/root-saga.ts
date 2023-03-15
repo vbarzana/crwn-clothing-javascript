0f7c014d33e309bebc24b3097dfe33f429af0a50
vbarzana <viticoinf@gmail.com>
2023-03-15 07:46:44 +0200

    fix: providing the project version only with javascript to be able to keep both

--- src/store/root-saga.ts
+++
@@ -1,8 +0,0 @@
-import { all, call } from 'typed-redux-saga/macro';
-
-import { categoriesSaga } from './categories/category.saga';
-import { userSagas } from './user/user.saga';
-
-export function* rootSaga() {
-  yield* all([call(categoriesSaga), call(userSagas)]);
-}
